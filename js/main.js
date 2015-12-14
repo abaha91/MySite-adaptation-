a4c792f42af562a2dee2342a044a16edfa623652
Gennady Abashkin <abashkin.91@mail.ru>
2015-12-15 00:20:44 +0300

    img-resize + some fixes

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var myModule = (function() {
+	
+	var init = function() {
+		_setUpListners();
+	};
+	
+	var _setUpListners = function() {
+		
+	}
+	
+	return {
+		init: init
+	};
+})();
+
+myModule.init();
