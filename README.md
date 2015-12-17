4637c5eaa90a31d6a735a9444f567be9ccb8d7b9
Gennady Abashkin <abashkin.91@mail.ru>
2015-12-17 22:34:53 +0300

    directory-wamp

---
+++ README.md
@@ -0,0 +1,7 @@
+Как установить мой проект<br />
+git clone https://github.com/abaha91/Project1.git site <br />
+cd site <br />
+bower i <br />
+npm i <br />
+gulp <br />
+
---
+++ js/main.js
@@ -0,0 +1,58 @@
+var myModule = (function() {
+	
+	var init = function() {
+		_setUpListners();
+	};
+	
+	var _setUpListners = function() {
+		$("#new-project-add").on("click", _showModal);//подключение модального окна
+		$(".project_but").on("click", _submitBut);//кнопка отправить
+		
+	};
+
+	var _showModal = function(ev) {
+		console.log('вызов модального окна');
+		ev.preventDefault();
+		$('#new_project_popup').bPopup();
+	};
+
+	var _submitBut = function(ev){
+		console.log('Кнопка отправить сработала');
+		ev.preventDefault();
+
+
+       //Объявляем переменные
+		var form = $(this);
+		    url = "add_project.php"
+		    data = form.serialize();
+
+		    console.log(data);
+
+
+			$.ajax({
+				url: '/path/to/file',
+				type: 'POST',
+				dataType: 'json',
+				data: data,
+			})
+			.done(function(ans) {
+				console.log("success");
+				console.log(ans);
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});
+			
+
+
+	};
+	
+	return {
+		init: init
+	};
+})();
+
+myModule.init();
