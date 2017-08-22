265fd578f53174bc57e1a61142490f3ae358cf54
Gennady <abashkin.91@mail.ru>
2017-08-22 18:04:10 +0300

    angular learning

---
+++ app/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    moduleId : module.id,
+    selector : 'headerModule',
+    templateUrl : 'header.component.html'
+})
+
+export class HeaderComponent {
+
+}
---
+++ app/skills.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector : 'skillsModule',
+    templateUrl : 'skills.component.ts',
+    moduleId : module.id
+})
+
+export class SkillsComponent {
+
+}
